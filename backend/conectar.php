652cbba51582c8bb6a73b9cd3a6eadd2499e10bf
CgtzNoemi <89617943+CgtzNoemi@users.noreply.github.com>
2023-04-03 17:37:47 -0600

    Update conectar.php

--- backend/conectar.php
+++ backend/conectar.php
@@ -1,9 +1,9 @@
 <?php
-$HOST = "localhost";
+$HOST = "containers-us-west-201.railway.app";
 $USER = "root";
-$PASSWORD = "root";
-$DB = "ajax";
-$PORT = 3306;
+$PASSWORD = "2xAtJTFNe6dvsYWuPmDh";
+$DB = "railway";
+$PORT = 6895;
 $conexion = mysqli_connect($HOST,$USER,$PASSWORD,$DB,$PORT);
 if(!$conexion){
     die('Error de conexión: '.mysqli_connect_errno());
