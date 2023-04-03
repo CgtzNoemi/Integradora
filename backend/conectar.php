b082f5a9b86969b8d803cf13c86a0c423d0db3cc
CgtzNoemi <89617943+CgtzNoemi@users.noreply.github.com>
2023-04-03 16:28:58 -0600

    Update conectar.php

--- backend/conectar.php
+++ backend/conectar.php
@@ -4,7 +4,7 @@ $USER = "root";
 $PASSWORD = "root";
 $DB = "ajax";
 $PORT = 3306;
-$conexion = mysqli_connect($HOST,$USER,$PASSWORD,$DB,$PORT);
+$conexion = mysqli_connect($HOST,$USER,$PASSWORD,$DB,$PORT,'utf8');
 if(!$conexion){
     die('Error de conexión: '.mysqli_connect_errno());
 }
